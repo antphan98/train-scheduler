46f06fc2c05c1d98f2b3b32e6701551b83f76b1f
An Phan <antphan98@gmail.com>
2019-11-04 20:41:07 -0600

    added train data object to store elements

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+$(document).on("click", "#submit", function (event) {
+event.preventDefault;
+
+let trainData = {
+    trainName: $("#inputTrain").val().trim(),
+    destination: $("#inputDestination").val().trim(),
+    trainTime: $("#inputTrainTime").val().trim(),
+    frequency: parseInt($("#inputFrequency").val().trim())
+
+}
+
+console.log(trainData);
+
+
+
+}
