efd3dec236da7d833bd6f0e66e6347ab81a1ca82
An Phan <antphan98@gmail.com>
2019-11-06 18:04:59 -0600

    deleted traindata object and added variables to .push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,4 @@
-
-  var firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyAnMKcmu_grEC7n94X5EjJ387SyglzTnWA",
     authDomain: "trainscheduler-39708.firebaseapp.com",
     databaseURL: "https://trainscheduler-39708.firebaseio.com",
@@ -7,49 +6,47 @@
     storageBucket: "trainscheduler-39708.appspot.com",
     messagingSenderId: "545866011970",
     appId: "1:545866011970:web:318bcd1f80ef1e760464db"
-  };
+};
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-  const database = firebase.database();
-  console.log(database);
+const database = firebase.database();
+console.log(database);
 
 
 $(document).on("click", "#submit", function (event) {
 
-    event.preventDefault;
-
-let trainData = {
-    trainName: $("#inputTrain").val().trim(),
-    destination: $("#inputDestination").val().trim(),
-    trainTime: $("#inputTrainTime").val().trim(),
-    frequency: parseInt($("#inputFrequency").val().trim()),
-    dateAdded: firebase.database.ServerValue.TIMESTAMP
-}
-
-console.log(trainData);
-database.ref().push(trainData);
-
-$("#inputTrain").val("");
-$("#inputDestination").val("");
-$("#inputTrainTime").val("");
-$("#inputFrequency").val("");
+            event.preventDefault;
 
 
-});
-
-database.ref().on("child_added", function(childSnapshot) {
-     const trainName = childSnapshot.val();
-     const destination = childSnapshot.val();
-     const trainTime = childSnapshot.val();
-     const frequency = childSnapshot.val();
+            const trainName = $("#inputTrain").val().trim();
+            const destination = $("#inputDestination").val().trim();
+            const trainTime = $("#inputTrainTime").val().trim();
+            const frequency = parseInt($("#inputFrequency").val().trim());
 
 
+            console.log(trainData);
+            database.ref().push({
 
-     $("#table").append(
-        ' <tr><td>' +  +
-        ' </td><td>' + trainName +
-        ' </td><td>' + destination +
-        ' </td><td>' + trainTime +
-        ' </td><td>' + frequency + ' </td></tr>');
-});
+                trainName: trainName,
+                destination: destination,
+                frequency: frequency,
+                trainTime: trainTime,
+                dateAdded: firebase.database.ServerValue.TIMESTAMP
+
+            });
+
+            database.ref().on("child_added", function (childSnapshot) {
+                const trainName = childSnapshot.val();
+                const destination = childSnapshot.val();
+                const trainTime = childSnapshot.val();
+                const frequency = childSnapshot.val();
+
+
+
+                $("#table").append(
+                    ' <tr><td>' + trainName +
+                    ' </td><td>' + destination +
+                    ' </td><td>' + trainTime +
+                    ' </td><td>' + frequency + ' </td></tr>');
+            });
